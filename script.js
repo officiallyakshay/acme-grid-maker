6f63eb5a4af1261e282417838ab6f581a87c81cb
casualshay <akshaysprabhakar@gmail.com>
2019-07-28 20:21:15 -0700

    some functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+const gridForm = document.querySelector('#form');
+const makeGrid = document.querySelector('#makeGridButton')
+
+gridForm.addEventListener('submit', (ev) => {
+	console.log(ev)
+	event.preventDefault();
+	if (event !== number) {
+		event.innerHTML = 'Choose a number from 1 to 50'
+	}
+});
+
+// const render = () => {
+
+// }
