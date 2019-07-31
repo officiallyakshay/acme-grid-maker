3644f653aa37241d9a1f14cbdddc5a31888f6b4e
casualshay <akshaysprabhakar@gmail.com>
2019-07-31 13:05:56 -0700

    fixing styling

--- script.js
+++ script.js
@@ -22,12 +22,12 @@ gridForm.addEventListener('keyup', (ev) => {
 	console.log(ev.target)
 	const row = +ev.target.value
 	const column = +ev.target.value
-	if (!row || row < 1 || row > 50) {
+	if (!row || row < 1 || row > 50 && ev.target.name === 'row') {
 		rowError.innerHTML = 'rows must be between 1 and 50';
 	} else {
 		rowError.innerHTML = '';
 	}
-	if (!column || column < 1 || column > 50) {
+	if (!column || column < 1 || column > 50 && ev.target.name === 'column') {
 		columnError.innerHTML = 'columns must be between 1 and 50';
 	} else {
 		columnError.innerHTML = '';
@@ -57,14 +57,11 @@ gridForm.addEventListener('keyup', (ev) => {
 		grid.innerHTML = html;
 	}
 	render();
-	
+
 
 	grid.addEventListener('click', (ev) => {
 		const rowVal = [...ev.target.parentNode.parentNode.children].indexOf(ev.target.parentNode);
 		const columnVal = [...ev.target.parentNode.children].indexOf(ev.target);
-		console.log('rIdx:', rowVal)
-		console.log('cIdx:', columnVal)
-		console.log(ev.target)
 		let html = 'rIdx: ' + rowVal + ' ' + 'cIdx: ' + columnVal
 		idx.innerHTML = html
 		ev.target.classList.toggle('highlight');
