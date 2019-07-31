de3628fc8acf61682c984b119e297ab6b713b57c
casualshay <akshaysprabhakar@gmail.com>
2019-07-30 20:03:35 -0700

    mostly complete

--- script.js
+++ script.js
@@ -3,24 +3,38 @@ const gridForm = document.querySelector('#form');
 const rowForm = document.querySelector('#row');
 const makeGrid = document.querySelector('#makeGridButton')
 const grid = document.getElementById("grid");
+const idx = document.querySelector("#indexClicked");
+const rowError = document.querySelector("#rowError");
+const columnError = document.querySelector("#columnError");
 let generatedGrid = [];
+
 
 gridForm.addEventListener('submit', (ev) => {
 	ev.preventDefault();
 	const row = +ev.target.row.value
-	console.log(row)
 	const column = +ev.target.column.value
-	console.log(column)
-	if (!row) {
-		ev.target.innerHTML += 'rows must be between 1 and 50';
-	}
-
-	if (!column) {
-		ev.target.innerHTML += 'columns must be between 1 and 50';
-	}
 	generatedGrid = generateGrid(row, column);
 	render();
 });
+
+gridForm.addEventListener('keyup', (ev) => {
+	ev.preventDefault();
+	console.log(ev.target)
+	const row = +ev.target.value
+	const column = +ev.target.value
+	if (!row || row < 1 || row > 50) {
+		rowError.innerHTML = 'rows must be between 1 and 50';
+	} else {
+		rowError.innerHTML = '';
+	}
+	if (!column || column < 1 || column > 50) {
+		columnError.innerHTML = 'columns must be between 1 and 50';
+	} else {
+		columnError.innerHTML = '';
+	}
+	render();	
+});
+
 		
 	function generateGrid(r, c) {
 		const rows = [];
@@ -36,20 +50,24 @@ gridForm.addEventListener('submit', (ev) => {
 		
 	const render = () => {
 		let html = generatedGrid.map((row) => {
-			return `<div class="row"> ${row.map((column) => {
+			return `<div class="row"> ${row.map(() => {
 				return `<div class="cell"> </div>`
 			}).join('')} </div>`
 		}).join('');
-		console.log(grid)
 		grid.innerHTML = html;
-		console.log(grid)
 	}
-	// render();
+	render();
+	
 
 	grid.addEventListener('click', (ev) => {
-		const column = [...ev.target.parentNode.children].indexOf(ev.target);
-		const row = [...ev.target.parentNode.parentNode.children].indexOf(ev.target.parentNode);
-		ev.target.classList.toggle('red');
+		const rowVal = [...ev.target.parentNode.parentNode.children].indexOf(ev.target.parentNode);
+		const columnVal = [...ev.target.parentNode.children].indexOf(ev.target);
+		console.log('rIdx:', rowVal)
+		console.log('cIdx:', columnVal)
+		console.log(ev.target)
+		let html = 'rIdx: ' + rowVal + ' ' + 'cIdx: ' + columnVal
+		idx.innerHTML = html
+		ev.target.classList.toggle('highlight');
 	});
-
-	// render();
+	render();
+	
